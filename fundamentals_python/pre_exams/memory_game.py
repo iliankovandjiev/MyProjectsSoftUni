932ce97b4d6a04b0c5fcca2d9b90f3d89053fabf
Ilian Kovandjiev <iliankovandjiev@gmail.com>
2022-10-13 16:05:22 +0300

    memory_game

---
+++ fundamentals_python/pre_exams/memory_game.py
@@ -0,0 +1,13 @@
+sequence_of_elements = input()
+lst_of_elements = []
+for index in range(0, len(sequence_of_elements), 2):
+    lst_of_elements.append(sequence_of_elements[index])
+integers_input = input()
+current_move = 0
+lst_of_sequence = []
+while integers_input != 'end':
+    for index in range(0, len(sequence_of_elements), 2):
+        lst_of_elements.append(sequence_of_elements[index])
+    lst_of_sequence.append(lst_of_sequence[1, 3])
+print(lst_of_elements)
+
