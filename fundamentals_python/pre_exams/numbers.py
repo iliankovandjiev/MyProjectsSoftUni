f8d0e8beba195a32a1ba81f7a3a72be20212c985
Ilian Kovandjiev <iliankovandjiev@gmail.com>
2022-10-17 17:54:48 +0300

    numbers.py

--- fundamentals_python/pre_exams/numbers.py
+++ fundamentals_python/pre_exams/numbers.py
@@ -6,27 +6,17 @@ final_list = []
 for num in int_numbers:
     if num > average_number:
         new_list.append(num)
-if new_list == 0:
+if len(new_list) == 0:
     print('No')
 else:
-    if len(new_list) > 5:
+    if len(new_list) > 4:
+        new_list = sorted(new_list, reverse=True)
+        result = [str(x) for x in new_list]
         for num in range(5):
-            final_list.append(new_list[num])
-        result = [str(x) for x in final_list]
+            final_list.append(result[num])
+        print(' '.join(final_list))
+
+    else:
+        new_list = sorted(new_list, reverse=True)
+        result = [str(x) for x in new_list]
         print(' '.join(result))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# if len(int_numbers) < 5:
-#     print('less than 5 numbers')
