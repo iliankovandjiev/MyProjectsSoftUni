00986efb587c3e99eebf9eee6e94907222d780a2
ilian.kovandzhiev <iliankovandjiev@gmail.com>
2022-11-04 15:23:40 +0200

    town

---
+++ fundamentals_python/classes_and_objects/town.py
@@ -0,0 +1,21 @@
+class Town:
+
+    def __init__(self, name: str, latitude="0°N", longitude="0°E"):
+        self.name = name
+        self.latitude = latitude
+        self.longitude = longitude
+
+    def set_latitude(self, latitude):
+        self.latitude = latitude
+
+    def set_longitude(self, longitude="0°E"):
+        self.longitude = longitude
+
+    def __repr__(self):
+        return f"Town: {self.name} | Latitude: {self.latitude} | Longitude: {self.longitude}"
+
+
+town = Town("Sofia")
+town.set_latitude("42° 41\' 51.04\" N")
+town.set_longitude("23° 19\' 26.94\" E")
+print(town)
