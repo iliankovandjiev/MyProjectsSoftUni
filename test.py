c852559bb842af25868ae4703fc1302fc1abfa7f
Ilian Kovandjiev <iliankovandjiev@gmail.com>
2023-03-28 20:02:19 +0300

    classes_and_objects

--- test.py
+++ test.py
@@ -1,9 +1,1 @@
-word1 = input()
-word2 = input()
-
-# How many letters does the longest word contain?
-
-if len(word1) > len(word2):
-    print len(word1)
-else:
-    print len(word2)
+print(type(x for x in "sometext"))
